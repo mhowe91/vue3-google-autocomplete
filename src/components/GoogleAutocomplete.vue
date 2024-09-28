155c3166908557a323d38048bb1f78a64d6bdf87
Matt Howe <mhowe@nordik.com>
2024-09-28 15:46:14 -0400

    Allowed for all Places API fields

--- src/components/GoogleAutocomplete.vue
+++ src/components/GoogleAutocomplete.vue
@@ -12,13 +12,36 @@ import { onMounted, ref, nextTick, onBeforeUnmount } from 'vue'
 
 const emit = defineEmits(['update:modelValue', 'set'])
 const props = defineProps({
-  modelValue: {
-    type: String,
-    default: ''
-  },
   apiKey: {
     type: String,
     required: true
+  },
+  class: {
+    type: String,
+    default: ''
+  },
+  fields: {
+    type: Array,
+    default: () => [
+      'place_id',
+      'formatted_address',
+      'address_components',
+      'geometry',
+      'name',
+      'rating',
+      'reviews',
+      'formatted_phone_number',
+      'user_ratings_total',
+      'website',
+    ],
+  },
+  isFullPayload: {
+    type: Boolean,
+    default: false
+  },
+  modelValue: {
+    type: String,
+    default: ''
   },
   placeholder: {
     type: String,
@@ -27,14 +50,6 @@ const props = defineProps({
   types: {
     type: Array,
     default: () => [],
-  },
-  isFullPayload: {
-    type: Boolean,
-    default: false
-  },
-  class: {
-    type: String,
-    default: ''
   }
 })
 
@@ -73,7 +88,7 @@ const setPlacesListener = () => {
   if (origin.value) {
     const places = google.maps.places
     const autocompleteInstance = new places.Autocomplete(origin.value, {
-      fields: ['formatted_address', 'address_components', 'geometry', 'name'],
+      fields: props.fields,
       types: props.types,
       strictBounds: false
     })
@@ -100,7 +115,43 @@ const setPlacesListener = () => {
         state: state,
         country: country,
         latitude: latitude,
-        longitude: longitude
+        longitude: longitude,
+        rating: place.value.rating || null,
+        reviews: place.value.reviews || [],
+        phone_number: place.value.formatted_phone_number || '',
+        international_phone_number: place.value.international_phone_number || '',
+        website: place.value.website || '',
+        opening_hours: place.value.opening_hours || null,
+        secondary_opening_hours: place.value.secondary_opening_hours || null,
+        address: place.value.formatted_address || '',
+        adr_address: place.value.adr_address || '',
+        photos: place.value.photos || [],
+        price_level: place.value.price_level || null,
+        user_ratings_total: place.value.user_ratings_total || null,
+        url: place.value.url || '',
+        business_status: place.value.business_status || null,
+        curbside_pickup: place.value.curbside_pickup || false,
+        delivery: place.value.delivery || false,
+        dine_in: place.value.dine_in || false,
+        editorial_summary: place.value.editorial_summary?.overview || '',
+        wheelchair_accessible_entrance: place.value.wheelchair_accessible_entrance || false,
+        icon: place.value.icon || '',
+        icon_background_color: place.value.icon_background_color || '',
+        icon_mask_base_uri: place.value.icon_mask_base_uri || '',
+        place_id: place.value.place_id || '',
+        types: place.value.types || [],
+        vicinity: place.value.vicinity || '',
+        serves_beer: place.value.serves_beer || false,
+        serves_breakfast: place.value.serves_breakfast || false,
+        serves_brunch: place.value.serves_brunch || false,
+        serves_dinner: place.value.serves_dinner || false,
+        serves_lunch: place.value.serves_lunch || false,
+        serves_vegetarian_food: place.value.serves_vegetarian_food || false,
+        serves_wine: place.value.serves_wine || false,
+        takeout: place.value.takeout || false,
+        reservable: place.value.reservable || false,
+        plus_code: place.value.plus_code?.global_code || '',
+        utc_offset: place.value.utc_offset || null,
       }
       emit('update:modelValue', place.value?.name)
       if (props.isFullPayload)
