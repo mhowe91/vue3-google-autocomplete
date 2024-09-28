9cd57ff0d2ab67404b0c55a56f96abf6b72b0bc0
Matt Howe <mhowe@nordik.com>
2024-09-28 15:46:41 -0400

    Build

--- dist/vue3-google-autocomplete.js
+++ dist/vue3-google-autocomplete.js
@@ -1,14 +1,37 @@
-import { defineComponent as A, ref as p, onMounted as P, nextTick as B, onBeforeUnmount as M, openBlock as S, createElementBlock as x, normalizeClass as b } from "vue";
-const C = ["placeholder"], G = /* @__PURE__ */ A({
+import { defineComponent as P, ref as c, onMounted as B, nextTick as M, onBeforeUnmount as S, openBlock as x, createElementBlock as C, normalizeClass as F } from "vue";
+const G = ["placeholder"], V = /* @__PURE__ */ P({
   __name: "GoogleAutocomplete",
   props: {
-    modelValue: {
-      type: String,
-      default: ""
-    },
     apiKey: {
       type: String,
       required: !0
+    },
+    class: {
+      type: String,
+      default: ""
+    },
+    fields: {
+      type: Array,
+      default: () => [
+        "place_id",
+        "formatted_address",
+        "address_components",
+        "geometry",
+        "name",
+        "rating",
+        "reviews",
+        "formatted_phone_number",
+        "user_ratings_total",
+        "website"
+      ]
+    },
+    isFullPayload: {
+      type: Boolean,
+      default: !1
+    },
+    modelValue: {
+      type: String,
+      default: ""
     },
     placeholder: {
       type: String,
@@ -17,76 +40,104 @@ const C = ["placeholder"], G = /* @__PURE__ */ A({
     types: {
       type: Array,
       default: () => []
-    },
-    isFullPayload: {
-      type: Boolean,
-      default: !1
-    },
-    class: {
-      type: String,
-      default: ""
     }
   },
   emits: ["update:modelValue", "set"],
-  setup(n, { emit: _ }) {
-    const s = _, i = n, c = p(), e = p(), d = p(!1), v = () => new Promise((t, o) => {
+  setup(n, { emit: b }) {
+    const r = b, o = n, i = c(), e = c(), d = c(!1), h = () => new Promise((a, s) => {
       if (window.google && window.google.maps && window.google.maps.places)
-        t();
+        a();
       else if (!d.value) {
         d.value = !0;
-        const l = document.createElement("script");
-        l.setAttribute(
+        const t = document.createElement("script");
+        t.setAttribute(
           "src",
-          `https://maps.googleapis.com/maps/api/js?key=${i.apiKey}&libraries=places&v=weekly&callback=initMap`
+          `https://maps.googleapis.com/maps/api/js?key=${o.apiKey}&libraries=places&v=weekly&callback=initMap`
         ), window.initMap = () => {
-          t();
-        }, l.onerror = async (r) => {
-          o(r);
-        }, document.head.appendChild(l);
+          a();
+        }, t.onerror = async (u) => {
+          s(u);
+        }, document.head.appendChild(t);
       }
-    }), h = () => {
-      if (c.value) {
-        const t = google.maps.places, o = new t.Autocomplete(c.value, {
-          fields: ["formatted_address", "address_components", "geometry", "name"],
-          types: i.types,
+    }), k = () => {
+      if (i.value) {
+        const a = google.maps.places, s = new a.Autocomplete(i.value, {
+          fields: o.fields,
+          types: o.types,
           strictBounds: !1
         });
-        o.addListener("place_changed", async () => {
-          var g, f, w;
-          e.value = await o.getPlace();
-          const l = await e.value.geometry.location.lat(), r = await e.value.geometry.location.lng();
-          let u = "", m = "", y = "";
-          for (const a of (g = e.value) == null ? void 0 : g.address_components)
-            a.types.includes("locality") ? u = await a.long_name : a.types.includes("administrative_area_level_1") ? m = await a.long_name : a.types.includes("country") && (y = await a.long_name);
-          const k = {
+        s.addListener("place_changed", async () => {
+          var m, f, g, y, w;
+          e.value = await s.getPlace();
+          const t = await e.value.geometry.location.lat(), u = await e.value.geometry.location.lng();
+          let _ = "", p = "", v = "";
+          for (const l of (m = e.value) == null ? void 0 : m.address_components)
+            l.types.includes("locality") ? _ = await l.long_name : l.types.includes("administrative_area_level_1") ? p = await l.long_name : l.types.includes("country") && (v = await l.long_name);
+          const A = {
             name: (f = e.value) == null ? void 0 : f.name,
-            city: u,
-            state: m,
-            country: y,
-            latitude: l,
-            longitude: r
+            city: _,
+            state: p,
+            country: v,
+            latitude: t,
+            longitude: u,
+            rating: e.value.rating || null,
+            reviews: e.value.reviews || [],
+            phone_number: e.value.formatted_phone_number || "",
+            international_phone_number: e.value.international_phone_number || "",
+            website: e.value.website || "",
+            opening_hours: e.value.opening_hours || null,
+            secondary_opening_hours: e.value.secondary_opening_hours || null,
+            address: e.value.formatted_address || "",
+            adr_address: e.value.adr_address || "",
+            photos: e.value.photos || [],
+            price_level: e.value.price_level || null,
+            user_ratings_total: e.value.user_ratings_total || null,
+            url: e.value.url || "",
+            business_status: e.value.business_status || null,
+            curbside_pickup: e.value.curbside_pickup || !1,
+            delivery: e.value.delivery || !1,
+            dine_in: e.value.dine_in || !1,
+            editorial_summary: ((g = e.value.editorial_summary) == null ? void 0 : g.overview) || "",
+            wheelchair_accessible_entrance: e.value.wheelchair_accessible_entrance || !1,
+            icon: e.value.icon || "",
+            icon_background_color: e.value.icon_background_color || "",
+            icon_mask_base_uri: e.value.icon_mask_base_uri || "",
+            place_id: e.value.place_id || "",
+            types: e.value.types || [],
+            vicinity: e.value.vicinity || "",
+            serves_beer: e.value.serves_beer || !1,
+            serves_breakfast: e.value.serves_breakfast || !1,
+            serves_brunch: e.value.serves_brunch || !1,
+            serves_dinner: e.value.serves_dinner || !1,
+            serves_lunch: e.value.serves_lunch || !1,
+            serves_vegetarian_food: e.value.serves_vegetarian_food || !1,
+            serves_wine: e.value.serves_wine || !1,
+            takeout: e.value.takeout || !1,
+            reservable: e.value.reservable || !1,
+            plus_code: ((y = e.value.plus_code) == null ? void 0 : y.global_code) || "",
+            utc_offset: e.value.utc_offset || null
           };
-          s("update:modelValue", (w = e.value) == null ? void 0 : w.name), i.isFullPayload ? s("set", e.value) : s("set", k);
+          r("update:modelValue", (w = e.value) == null ? void 0 : w.name), o.isFullPayload ? r("set", e.value) : r("set", A);
         });
       }
     };
-    return P(async () => {
+    return B(async () => {
       try {
-        await v(), await B(), h();
-      } catch (t) {
-        console.error("Failed to load Google Maps API", t);
+        await h(), await M(), k();
+      } catch (a) {
+        console.error("Failed to load Google Maps API", a);
       }
-    }), M(() => {
+    }), S(() => {
       delete window.initMap;
-    }), (t, o) => (S(), x("input", {
+    }), (a, s) => (x(), C("input", {
       ref_key: "origin",
-      ref: c,
+      ref: i,
       type: "text",
-      class: b(n.class),
+      class: F(n.class),
       placeholder: n.placeholder
-    }, null, 10, C));
+    }, null, 10, G));
   }
 });
 export {
-  G as GoogleAutocomplete
+  V as GoogleAutocomplete
 };
